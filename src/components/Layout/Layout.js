752ed7c59a9d1e1322498d3e47471e25d596ec45
AndreyKonovalenko <andreinew@mail.ru>
2021-04-05 23:11:50 +0300

    chore: work on styles

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,31 +1,36 @@
-import { useMediaQuery } from 'react-responsive';
+import {useMediaQuery} from 'react-responsive';
+import cssObject from './Layout.css';
 
 const Layout = (props) => {
   const isDesktopOrLaptop = useMediaQuery({
-    query: '(min-device-width: 1224px)'
-  })
-  const isBigScreen = useMediaQuery({ query: '(min-device-width: 1824px)' })
-  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+    query: '(min-device-width: 1224px)',
+  });
+  const isBigScreen = useMediaQuery({query: '(min-device-width: 1824px)'});
+  const isTabletOrMobile = useMediaQuery({query: '(max-width: 1224px)'});
   const isTabletOrMobileDevice = useMediaQuery({
-    query: '(max-device-width: 1224px)'
-  })
-  const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
-  const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
+    query: '(max-device-width: 1224px)',
+  });
+  const isPortrait = useMediaQuery({query: '(orientation: portrait)'});
+  const isRetina = useMediaQuery({query: '(min-resolution: 2dppx)'});
 
   return (
-    <div>
+    <div className={cssObject.Main}>
       <h1>Device Test!</h1>
-      {isDesktopOrLaptop && <>
-        <p>You are a desktop or laptop</p>
-        {isBigScreen && <p>You also have a huge screen</p>}
-        {isTabletOrMobile && <p>You are sized like a tablet or mobile phone though</p>}
-      </>}
+      {isDesktopOrLaptop && (
+        <>
+          <p>You are a desktop or laptop</p>
+          {isBigScreen && <p>You also have a huge screen</p>}
+          {isTabletOrMobile && (
+            <p>You are sized like a tablet or mobile phone though</p>
+          )}
+        </>
+      )}
       {isTabletOrMobileDevice && <p>You are a tablet or mobile phone</p>}
       <p>Your are in {isPortrait ? 'portrait' : 'landscape'} orientation</p>
       {isRetina && <p>You are retina</p>}
       {props.children}
     </div>
   );
-}
+};
 
 export default Layout;
