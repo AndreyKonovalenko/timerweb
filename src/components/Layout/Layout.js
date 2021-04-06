c10554871d3130e1b46370a0fce2e6a545a60f6c
EC2 Default User <andreinew@mail.ru>
2021-04-06 07:46:38 +0000

    chore: unlock css module

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,17 @@
-import {useMediaQuery} from 'react-responsive';
-import cssObject from './Layout.css';
+import { useMediaQuery } from 'react-responsive';
+import cssObject from './Layout.module.css';
 
 const Layout = (props) => {
   const isDesktopOrLaptop = useMediaQuery({
     query: '(min-device-width: 1224px)',
   });
-  const isBigScreen = useMediaQuery({query: '(min-device-width: 1824px)'});
-  const isTabletOrMobile = useMediaQuery({query: '(max-width: 1224px)'});
+  const isBigScreen = useMediaQuery({ query: '(min-device-width: 1824px)' });
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' });
   const isTabletOrMobileDevice = useMediaQuery({
     query: '(max-device-width: 1224px)',
   });
-  const isPortrait = useMediaQuery({query: '(orientation: portrait)'});
-  const isRetina = useMediaQuery({query: '(min-resolution: 2dppx)'});
+  const isPortrait = useMediaQuery({ query: '(orientation: portrait)' });
+  const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' });
 
   return (
     <div className={cssObject.Main}>
