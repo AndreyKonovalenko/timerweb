a7c513e6ab2eac8cb096e94cff8cf69828b64a10
AndreyKonovalenko <andreinew@mail.ru>
2021-04-11 13:19:29 +0300

    chore: working on basic timer logic'

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,9 +1,55 @@
+import React, {useState, useEffect} from 'react';
+
+//   let dist = distance;
+//   return (dispatch) => {
+//     let id = setInterval(() => {
+//       if (dist > 0) {
+//         dist = dist - 100;
+//         // interval controller
+//         intervalController(dist, currentTimer, sounds);
+//         dispatch(setDistance(dist));
+//       }
+//     }, 100);
+//     dispatch(setTimerIdentifyingValue(id));
+//   };
+// };
+
 const Timer = () => {
+  const [distance, setDistance] = useState(6000);
+  const [timerId, setTimerId] = useState(null);
+
+  const onStartHandler = (distance) => {
+    let dist = distance;
+    let id = setInterval(() => {
+      if (dist > 0) {
+        dist = dist - 100;
+        setDistance(dist);
+      }
+    }, 100);
+    setTimerId(id);
+  };
+
+  const onStopHandler = (timerID) => {
+    clearInterval(timerId);
+    console.log('stop');
+  };
+
+  useEffect(() => {
+    console.log('timerId: ', timerId, 'time left: ', distance);
+    if (distance === 0) {
+      console.log('reset Timer');
+      clearInterval(timerId);
+      setDistance(6000);
+    }
+  }, [distance, timerId]);
+
   return (
     <div>
-      <p>00:00</p>
+      <p>{distance}</p>
+      <button onClick={() => onStartHandler(distance)}>start</button>
+      <button onClick={() => onStopHandler(timerId)}>stop</button>
     </div>
   );
-}
+};
 
 export default Timer;
