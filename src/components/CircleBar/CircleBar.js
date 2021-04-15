2dac247421d306fad39cdbbd8821ea76b4390997
EC2 Default User <andreinew@mail.ru>
2021-04-15 13:53:59 +0000

    chore: added constants file

---
+++ src/components/CircleBar/CircleBar.js
@@ -0,0 +1,5 @@
+const CircleBar = () => {
+
+}
+
+export default CircleBar;
---
+++ src/constants/colors.js
@@ -0,0 +1,7 @@
+const Colors = {
+  first: '#e38c4c',
+  second: '#f6c651',
+  third: '#fcf1da',
+  fourth: '#977b60'
+};
+export default Colors;
